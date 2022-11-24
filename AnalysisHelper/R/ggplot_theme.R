1cd309feef384552ebc7f1c6f86be3f1465ffe29
hiroakif93 <73891758+hiroakif93@users.noreply.github.com>
2022-11-24 21:11:06 +0900

    Add files via upload

---
+++ AnalysisHelper/R/ggplot_theme.R
@@ -0,0 +1,35 @@
+#' ggplot2 theme
+#' 
+#' @description ggplot2 theme
+#' 
+#' @param bsize : mimum size of text.
+#' @param lwd : linewidth in element_rect.
+#' @param unitsize : legend.key.size.
+#' @param family : font family.
+#'
+#' 
+#' @examples
+#'
+#' @export
+
+
+theme_text <- function(bsize=6, lwd=0.5, unitsize=1, family=NULL){
+    
+    themeown <- theme(plot.background = element_blank(),
+                       panel.border=element_rect(fill=NA, size=lwd),
+                       panel.background = element_rect(fill = NA, 
+                                                       colour = NA),
+                       axis.text=element_text(size=bsize, family=family),
+                       axis.title=element_text(size=bsize+1, family=family),
+                       strip.background = element_blank(),
+                       strip.text=element_text(size=bsize+1, family=family),
+                       legend.background = element_blank(),
+                       legend.text = element_text(size=bsize, family=family),
+                       legend.title = element_text(size=bsize+1, family=family),
+                       legend.key.size=unit(unitsize, 'line'),
+                       title = element_text(size=bsize+1, family=family),
+                       plot.subtitle = element_text(size=bsize+1, family=family),
+                       plot.caption = element_text(size=bsize+1, family=family)
+                       )
+    return(list(themeown))
+}
