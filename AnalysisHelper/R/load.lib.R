271ebc3f08f6e5c9f6942ba2e16a31caf148bdd3
hiroakif93 <73891758+hiroakif93@users.noreply.github.com>
2026-04-01 15:29:36 +0900

    Improve load.lib function and its documentation
    Refactor load.lib function to improve parameter handling and documentation.

--- AnalysisHelper/R/load.lib.R
+++ AnalysisHelper/R/load.lib.R
@@ -1,27 +1,32 @@
 #' Loading multiple library
-#' 
+#'
 #' @param ...: package names. Quotations are unnecessary.
-#' 
+#'
 #' @examples
 #' load.lib( "vegan" )
 #' load.lib( c("vegan", "ggplot2") )
-#' 
+#' load.lib( vegan, ggplot2 )
 #' @export
 
-load.lib <- function (...) 
-{	
+load.lib <- function (...)
+{
 
-	# -------------------------------------------------#	
-    # Loading multiple library function.
-    # libs is a character vector of package names.
-    #
-    # Exnample 
-    # load.lib( vegan )
-    # -------------------------------------------------#
-    
-   libs <- as.character(substitute(list(...)))[-1]
-  
+  # -------------------------------------------------#
+  # Loading multiple library function.
+  # libs is a character vector of package names.
+  #
+  # Exnample
+  # load.lib( vegan )
+  # -------------------------------------------------#
+  args <- list(...)
+
+  if (length(args) == 1 && is.vector(args[[1]])) {
+    libs = args[[1]]
+  } else {
+    libs = as.character(substitute(list(...)))[-1]
+  }
+
   invisible(lapply(libs, function(x) invisible(library(package = x, character.only = TRUE))))
   invisible(sapply(libs, function(x) cat(sprintf("%s %s\n", x, packageVersion(x)))))
-    
+
 }
